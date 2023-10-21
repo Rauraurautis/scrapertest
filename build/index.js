632bf4bce3a3bc4ec673f06945da0797266e2eec
Rauraurautis <rautanokka@gmail.com>
2023-10-21 04:11:53 +0300

    asd

--- build/index.js
+++ build/index.js
@@ -5,16 +5,15 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const routes_1 = require("./routes");
+const cors_1 = __importDefault(require("cors"));
 const dotenv_1 = require("dotenv");
 const cookie_parser_1 = __importDefault(require("cookie-parser"));
 (0, dotenv_1.config)();
 const app = (0, express_1.default)();
-app.use(express_1.default.json());
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-    res.setHeader('Access-Control-Allow-Credentials', 'true');
-    next();
-});
+app.use((0, cors_1.default)({
+    origin: 'https://scraper-react-app.onrender.com',
+    credentials: true,
+}));
 app.use(express_1.default.static("public"));
 app.use((0, cookie_parser_1.default)());
 app.listen(process.env.PORT, () => {
--- index.ts
+++ index.ts
@@ -9,7 +9,7 @@ const app = express()
 
 
 app.use(cors({
-    origin: 'https://scraper-react-app.onrender.com',
+    origin: ['https://scraper-react-app.onrender.com', "http://localhost:3000"],
     credentials: true,
 }));
 app.use(express.static("public"))
