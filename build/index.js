11c67c03bfacfc239b198af8c9fc5492835bd441
Rauraurautis <rautanokka@gmail.com>
2023-10-21 04:17:30 +0300

    asd

--- build/index.js
+++ build/index.js
@@ -11,7 +11,7 @@ const cookie_parser_1 = __importDefault(require("cookie-parser"));
 (0, dotenv_1.config)();
 const app = (0, express_1.default)();
 app.use((0, cors_1.default)({
-    origin: 'https://scraper-react-app.onrender.com',
+    origin: ['https://scraper-react-app.onrender.com', "http://localhost:3000"],
     credentials: true,
 }));
 app.use(express_1.default.static("public"));
