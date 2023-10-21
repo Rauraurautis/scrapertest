3618422f9648343125b4cafe39bd60ba3c32b4e7
Rauraurautis <rautanokka@gmail.com>
2023-10-21 03:56:52 +0300

    asd

--- index.ts
+++ index.ts
@@ -8,12 +8,10 @@ const app = express()
 
 
 
-app.use(express.json())
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'https://scraper-react-app.onrender.com/test');
-    res.setHeader('Access-Control-Allow-Credentials', 'true');
-    next();
-  });
+  app.use(cors({
+    origin: 'https://scraper-react-app.onrender.com/test',
+    credentials: true,
+}));
 app.use(express.static("public"))
 app.use(cookieParser())
 
